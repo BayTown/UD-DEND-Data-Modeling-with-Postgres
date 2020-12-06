e98f89b7e3cba1ae441065a8b60dfcaeb02da2e1
BayTown <andi86h@outlook.com>
2020-12-06 21:38:12 +0100

    working on etl.ipynb

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,7 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=udacity user=student password=student")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
